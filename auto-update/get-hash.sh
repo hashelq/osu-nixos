cf423bad5002f10b364793f11298262e3628cf1d
hashelq <105644136+hashelq@users.noreply.github.com>
2024-01-09 12:44:52 +0000

    Update get-hash.sh

--- auto-update/get-hash.sh
+++ auto-update/get-hash.sh
@@ -9,5 +9,4 @@ url=$(curl -sL https://api.github.com/repos/ppy/osu/releases/latest | ./jq '.ass
 curl -sL $url > osu.AppImage
 chmod +x ./osu.AppImage
 ./osu.AppImage --appimage-extract
-ls
 md5sum bin/osu.Game.dll
