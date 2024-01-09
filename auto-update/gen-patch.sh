c1a211127ed078420e36d08dd5fff67242fedd67
hashelq <105644136+hashelq@users.noreply.github.com>
2024-01-09 12:45:08 +0000

    Update gen-patch.sh

--- auto-update/gen-patch.sh
+++ auto-update/gen-patch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 hash=$1
-part1=$(cat part1)
+part1=$(cat auto-update/part1)
 part1=${part1::-1}
-part2=$(cat part2)
+part2=$(cat auto-update/part2)
 part2=${part2::-1}
-echo "$part1$hash$part2" > ../bypass-tamper-detection.patch
+echo "$part1$hash$part2" > bypass-tamper-detection.patch
