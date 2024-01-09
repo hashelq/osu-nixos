5387ce453651789fe95c071a8f739c49b4362914
hashelq <105644136+hashelq@users.noreply.github.com>
2024-01-09 12:42:08 +0000

    Update get-hash.sh

--- auto-update/get-hash.sh
+++ auto-update/get-hash.sh
@@ -7,6 +7,6 @@ curl -sL https://github.com/jqlang/jq/releases/download/jq-1.7/jq-linux-amd64 > 
 chmod +x ./jq
 url=$(curl -sL https://api.github.com/repos/ppy/osu/releases/latest | ./jq '.assets .[] | select(.name == "osu.AppImage") .browser_download_url' | tr -d '\"')
 curl -sL $url > osu.AppImage
-chmod +x /osu.AppImage
+chmod +x ./osu.AppImage
 ./osu.AppImage --appimage-extract
 md5sum bin/osu.Game.dll
