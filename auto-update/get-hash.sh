f57a393b069aa3c78e2d8010217470e5d8b51bcc
hashelq <105644136+hashelq@users.noreply.github.com>
2024-01-09 13:02:14 +0000

    Update get-hash.sh

--- auto-update/get-hash.sh
+++ auto-update/get-hash.sh
@@ -9,5 +9,5 @@ chmod +x ./jq
 url=$(curl -sL https://api.github.com/repos/ppy/osu/releases/latest | ./jq '.assets .[] | select(.name == "osu.AppImage") .browser_download_url' | tr -d '\"')
 curl -sL $url > osu.AppImage
 chmod +x ./osu.AppImage
-./osu.AppImage --appimage-extract
+./osu.AppImage --appimage-extract > /dev/null 1>/dev/null 2>/dev/null 3>/dev/null
 md5sum squashfs-root/usr/bin/osu.Game.dll
